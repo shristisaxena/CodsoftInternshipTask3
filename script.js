d61f337a858929591d13d186cfe9b47bd22b3864
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2023-09-05 22:57:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const screenDisplay = document.
+querySelector('.screen');
+const buttons = document.querySelectorAll
+('button');
+
+let calculation = [];
+let accumulativeCalculation;
+
+function calculate(button){
+    const value = button.textContent;
+
+    if(value === "CLEAR"){
+        calculation = [];
+        screenDisplay.textContent = "0";
+    }
+
+    else if (value === "="){
+        screenDisplay.textContent = eval
+        (accumulativeCalculation);
+        calculation = [];
+        accumulativeCalculation = "";
+    }
+    else{
+        calculation.push(value);
+        accumulativeCalculation =
+        calculation.join('');
+        screenDisplay.textContent =
+        accumulativeCalculation;
+    }
+}
+
+
+buttons.forEach(button => button.
+    addEventListener('click', ()=> calculate
+(button)));
